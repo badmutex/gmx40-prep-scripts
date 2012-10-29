a3890c4b885e7098906a628e075f21dc1f87174c
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-10-29 15:39:16 -0400

    allow prep script to be called from anywhere

--- prepare-gmx40.sh
+++ prepare-gmx40.sh
@@ -4,13 +4,23 @@ set -e
 
 inputpdb=$1
 
+locate() {
+	name=$1
+	echo $(dirname $0)/$name
+}
 
-./removeHydrogends.py -i $inputpdb -o 00-nohydrogens.pdb
-./passThroughTleap.py -f 00-nohydrogens.pdb -o 01-tleap.pdb
+REMOVE_HBONDS=$(locate removeHydrogens.py)
+PASS_THROUGH_TLEAP=$(locate passThroughTleap.py)
+RESIDUE_SUBSTITUTIONS=$(locate residueSubstitutions.py)
+FIX_HYDROGENS=$(locate fixHydrogens.py)
+
+
+$REMOVE_HBONDS -i $inputpdb -o 00-nohydrogens.pdb
+$PASS_THROUGH_TLEAP -f 00-nohydrogens.pdb -o 01-tleap.pdb
 sed -i '/^TER$/d' 01-tleap.pdb
-./residueSubstitutions.py -i 01-tleap.pdb -o 02-subst.pdb \
+$RESIDUE_SUBSTITUTIONS -i 01-tleap.pdb -o 02-subst.pdb \
 	-a 'LYS     1' 'NLYP    1' \
 	-a 'ALA     1' 'NALA    1' \
 	-a ' LYS ' ' LYP '
-./fixHydrogens.py -f 02-subst.pdb -o 03-fixed.pdb
+$FIX_HYDROGENS -f 02-subst.pdb -o 03-fixed.pdb
 pdb2gmx -f 03-fixed.pdb -ff amber03
